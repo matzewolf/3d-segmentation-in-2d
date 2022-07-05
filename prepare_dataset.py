c05e040ab3858e5d8ec30818387d7b958f3a598f
Matthias Wolf <matze.wolf@tum.de>
2022-07-05 17:35:30 +0200

    Add final preparation step for data

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 
 import h5py
+import numpy as np
+
+from utils import jitter_point_cloud, gpgl2_segmentation
 
 
-def prepare_split(split: str, files: list[Path], result_file: h5py.File):
+def combine_split(split: str, files: list[Path], result_file: h5py.File):
     assert split in ['train', 'val', 'test'], ValueError("Invalid split")
 
     split_length = 0
@@ -12,23 +15,34 @@ def prepare_split(split: str, files: list[Path], result_file: h5py.File):
             split_length += len(f['label'])
     print(f"{split} set length: {split_length}")
 
-    points = result_file.create_dataset(f"x_{split}", (split_length, 2048, 3), dtype='<f4')
-    label = result_file.create_dataset(f"y_{split}", (split_length, 1), dtype='|u1')
-    part = result_file.create_dataset(f"s_{split}", (split_length, 2048), dtype='|u1')
+    points_3d = result_file.create_dataset(
+        f"x_{split}",
+        (split_length, 2048, 3),
+        dtype='<f4'
+    )
+    labels = result_file.create_dataset(
+        f"y_{split}",
+        (split_length, 1),
+        dtype='|u1'
+    )
+    parts = result_file.create_dataset(
+        f"s_{split}",
+        (split_length, 2048),
+        dtype='|u1'
+    )
 
     offset = 0
     for file in files:
         with h5py.File(file, 'r') as f:
             print(f"combining dataset: {file.name}")
             dataset_length = len(f['label'])
-            points[offset:offset + dataset_length] = f['data']
-            label[offset:offset + dataset_length] = f['label']
-            part[offset:offset + dataset_length] = f['pid']
+            points_3d[offset:offset + dataset_length] = f['data']
+            labels[offset:offset + dataset_length] = f['label']
+            parts[offset:offset + dataset_length] = f['pid']
             offset += dataset_length
 
 
-def prepare_dataset():
-    data_path = Path('hdf5_data')
+def combine_dataset(data_path: Path, combined_hdf5_file: Path):
     train_files = []
     val_files = []
     test_files = []
@@ -40,11 +54,87 @@ def prepare_dataset():
         elif file.name.startswith("ply_data_test"):
             test_files.append(file)
 
-    with h5py.File('shapenet_combined.h5', 'w') as result_file:
-        prepare_split('train', train_files, result_file)
-        prepare_split('val', val_files, result_file)
-        prepare_split('test', test_files, result_file)
+    with h5py.File(combined_hdf5_file, 'w') as result_file:
+        combine_split('train', train_files, result_file)
+        combine_split('val', val_files, result_file)
+        combine_split('test', test_files, result_file)
+
+
+def prepare_split(split: str, input_file: Path, result_file: h5py.File, num_repeats_train: int = 5,
+                  jitter_train_points: bool = True):
+    assert split in ['train', 'val', 'test'], ValueError("Invalid split")
+
+    if split == 'train':
+        num_repeats = num_repeats_train
+    else:
+        num_repeats = 1
+
+    with h5py.File(input_file, 'r') as f:
+        num_instances, num_points = f[f"s_{split}"].shape
+        points_3d = f[f"x_{split}"][:]
+        labels = f[f"y_{split}"][:]
+        parts = f[f"s_{split}"][:]
+
+    result_points_3d = result_file.create_dataset(
+        f"x_{split}",
+        (num_instances * num_repeats, num_points, 3),
+        dtype='f'
+    )
+    result_labels = result_file.create_dataset(
+        f"y_{split}",
+        (num_instances * num_repeats),
+        dtype='i'
+    )
+    result_parts = result_file.create_dataset(
+        f"s_{split}",
+        (num_instances * num_repeats, num_points),
+        dtype='i'
+    )
+    result_points_2d = result_file.create_dataset(
+        f"p_{split}",
+        (num_instances * num_repeats, num_points, 2),
+        dtype='i'
+    )
+
+    sample_idx = 0
+    node_losses = []
+    for repeat_idx in range(num_repeats):
+        for instance_idx in range(num_instances):
+            current_points_3d = points_3d[instance_idx]
+            current_label = labels[instance_idx]
+            current_parts = parts[instance_idx]
+            current_points_3d = current_points_3d[np.newaxis, :, :]
+            if jitter_train_points and split == 'train':
+                current_points_3d = jitter_point_cloud(current_points_3d)
+            current_points_2d, node_loss = gpgl2_segmentation(current_points_3d[0])
+            result_points_3d[sample_idx] = current_points_3d
+            result_labels[sample_idx] = current_label
+            result_parts[sample_idx] = current_parts
+            result_points_2d[sample_idx] = current_points_2d
+            sample_idx += 1
+            node_losses.append(node_loss)
+            print(f"Split {split}: iteration {sample_idx}, loss {node_loss}")
+
+    mean_node_loss = np.array(node_losses).mean()
+    result_file[f"num_repeats_{split}"] = num_repeats
+    result_file[f"mean node loss_{split}"] = mean_node_loss
+    print(f"{split} loss: {mean_node_loss}")
+
+
+def prepare_dataset(combined_hdf5_file: Path, prepared_hdf5_file: Path):
+    with h5py.File(prepared_hdf5_file, 'w') as result_file:
+        prepare_split('train', combined_hdf5_file, result_file)
+        prepare_split('val', combined_hdf5_file, result_file)
+        prepare_split('test', combined_hdf5_file, result_file)
+
+
+def main():
+    data_path = Path('hdf5_data')
+    combined_hdf5_file = Path('shapenet_combined.h5')
+    prepared_hdf5_file = Path('shapenet_prepared_test.h5')
+    combine_dataset(data_path, combined_hdf5_file)
+    prepare_dataset(combined_hdf5_file, prepared_hdf5_file)
 
 
 if __name__ == '__main__':
-    prepare_dataset()
+    main()
---
+++ utils.py
@@ -0,0 +1,166 @@
+import networkx as nx
+import numpy as np
+from numpy.typing import NDArray
+from scipy.spatial.distance import cdist, pdist, squareform
+from sklearn.cluster import KMeans
+
+
+def jitter_point_cloud(points_3d: NDArray, sigma: float = 0.01,
+                       clip: float = 0.05) -> NDArray:
+    """
+    Randomly jitters each point independently.
+
+    :param points_3d: BxNx3 array, original batch of point clouds
+    :param sigma: Variance of the jitter
+    :param clip: maximum absolute value of the jitter
+    :return: BxNx3 array, jittered batch of point clouds
+    """
+    assert clip > 0
+    return points_3d + np.clip(sigma * np.random.randn(*points_3d.shape), -clip, clip)
+
+
+def gpgl2_segmentation(points_3d: NDArray, num_cuts: int = 32,
+                       size_sub: int = 16, size_top: int = 16) -> tuple[NDArray, float]:
+    k_means = KMeans(n_clusters=num_cuts, n_init=1, max_iter=100)
+    points_3d = points_3d + np.random.rand(*points_3d.shape) * 1e-6
+    num_points = points_3d.shape[0]
+    dist_mat = k_means.fit_transform(points_3d)
+
+    node_top, labels = graph_cut(points_3d, dist_mat, num_points, num_cuts)
+
+    aij_mat = squareform(pdist(node_top), checks=False)
+    H = nx.from_numpy_matrix(aij_mat)
+    pos_spring = nx.spring_layout(H)
+    pos_spring = np.array([pos for idx, pos in sorted(pos_spring.items())])
+    pos_top = gpgl_layout_push(pos_spring, size_top)
+
+    pos_cuts = []
+    for i_cut in range(num_cuts):
+        pos_cut_3D = points_3d[labels == i_cut, :]
+
+        if len(pos_cut_3D) < 5:
+            pos_raw = [[0, 0], [0, 1], [1, 1], [1, 0]]
+            pos = pos_raw[:len(pos_cut_3D)]
+            pos_cuts.append(pos)
+            continue
+
+        aij_mat = squareform(pdist(pos_cut_3D), checks=False)
+        H = nx.from_numpy_matrix(aij_mat)
+        pos_spring = nx.spring_layout(H)
+        pos_spring = np.array([pos for idx, pos in sorted(pos_spring.items())])
+        pos = gpgl_layout_push(pos_spring, size_sub)
+
+        pos_cuts.append(pos)
+
+    # combine all layout positions
+    cuts_count = np.zeros(num_cuts).astype(np.int64)
+    pos_all = []
+    for idx in range(num_points):
+        label = labels[idx]
+        pos_all.append(pos_cuts[label][cuts_count[label]] + pos_top[label] * size_sub)
+        cuts_count[label] += 1
+    pos_all = np.array(pos_all)
+
+    num_nodes_m = len(np.unique(pos_all, axis=0))
+    node_loss_rate = (1 - num_nodes_m / num_points)
+    return pos_all, node_loss_rate
+
+
+def graph_cut(data: NDArray, dist_mat: NDArray,
+              num_points: int, num_cuts: int) -> tuple[NDArray, NDArray]:
+    num_cutpoints = int(num_points / num_cuts)
+    cutpoints_threshold = np.ceil(num_cutpoints * 1.2)
+    cluster = np.argmin(dist_mat, axis=-1)
+    mask = np.zeros([num_points, num_cuts])
+    for m, c in zip(mask, cluster):
+        m[c] = 1
+    flow_mat = np.zeros([num_cuts, num_cuts])
+
+    # separate point cloud into num_cuts clusters
+    for i_loop in range(500):
+        loss_mask = mask.sum(0)
+        order_list = np.argsort(loss_mask)
+        if loss_mask.max() <= cutpoints_threshold + 1:
+            break
+        for i_order, order in zip(range(len(order_list)), order_list):
+            if loss_mask[order] > cutpoints_threshold:
+                idxs = np.where(mask[:, order])[0]
+                idys_ori = order_list[:i_order]
+                idys = []
+                for idy in idys_ori:
+                    if flow_mat[order, idy] >= 0:
+                        idys.append(idy)
+
+                mat_new = dist_mat[idxs, :]
+                mat_new = mat_new[:, idys]
+                cost_list_row = mat_new.argmin(-1)
+                cost_list_col = mat_new.min(-1)
+
+                row = cost_list_col.argmin(-1)
+                col = cost_list_row[row]
+
+                target_idx = [idxs[row], idys[col]]
+                mask[target_idx[0], order] = 0
+                mask[target_idx[0], target_idx[1]] = 1
+                flow_mat[order, target_idx[1]] = 1
+                flow_mat[target_idx[1], order] = -1
+
+    center_pos = []
+    for i_cut in range(num_cuts):
+        if mask[:, i_cut].sum() > 0:
+            center_pos.append(data[mask[:, i_cut].astype(np.bool), :].mean(0))
+        else:
+            center_pos.append([0, 0])
+    labels = mask.argmax(-1)
+    return np.array(center_pos), labels
+
+
+def gpgl_layout_push(pos: NDArray, size: int) -> NDArray:
+    dist_mat = pdist(pos)
+    scale1 = 1 / dist_mat.min()
+    scale2 = (size-2) / (pos.max() - pos.min())
+    scale = np.min([scale1, scale2])
+    pos *= scale
+
+    pos_quat = np.round(pos).astype(np.int)
+    pos_quat = pos_quat - np.min(pos_quat, axis=0) + np.array([1, 1])
+    pos_unique, count = np.unique(pos_quat, axis=0, return_counts=True)
+    # node_loss = np.sum(count)-len(count)
+    # print('node_loss',node_loss)
+
+    mask = np.zeros((size, size)).astype(np.int)
+    for pt in pos_quat:
+        mask[pt[0], pt[1]] += 1
+
+    for i_loop in range(50):
+        if mask.max() <= 1:
+            # print("early stop")
+            break
+        idxs = np.where(count > 1)[0]
+        for idx in idxs:
+            pos_overlap = pos_unique[idx]
+            dist = cdist(pos_quat, [pos_overlap])
+            idy = np.argmin(dist)
+
+            b_down = np.maximum(pos_overlap[0] - 1, 0)
+            b_up = np.minimum(pos_overlap[0] + 2, size)
+            b_left = np.maximum(pos_overlap[1] - 1, 0)
+            b_right = np.minimum(pos_overlap[1] + 2, size)
+
+            mask_target = mask[b_down:b_up, b_left:b_right]
+            if mask_target.min() == 0:
+                pos_target = np.unravel_index(np.argmin(mask_target), mask_target.shape)
+                pos_mask = pos_target + np.array([b_down, b_left])
+            else:
+                pos_empty = np.array(np.where(mask == 0)).T
+                dist = cdist(pos_empty, [pos_overlap])
+                pos_target = pos_empty[np.argmin(dist)]
+                direction = pos_target - pos_overlap
+                direction1 = np.round(direction / np.linalg.norm(direction))
+                pos_mask = pos_overlap + direction1.astype(np.int)
+
+            pos_quat[idy] = pos_mask
+            mask[pos_overlap[0], pos_overlap[1]] -= 1
+            mask[pos_mask[0], pos_mask[1]] += 1
+            pos_unique, count = np.unique(pos_quat, axis=0, return_counts=True)
+    return pos_quat
