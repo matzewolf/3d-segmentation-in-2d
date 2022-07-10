4faccdfd22d5af5cb0698aeaf47c0e502e071b4a
Matthias Wolf <34787753+matzewolf@users.noreply.github.com>
2022-07-10 15:52:41 +0200

    Data preparation and PyTorch dataset implementation (#1)

---
+++ dataset.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import h5py
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
+
+from prepare_dataset import combine_dataset, prepare_dataset
+
+
+class ShapeNetPartDataset(Dataset):
+    """
+    PyTorch dataset class for the Stanford ShapeNet Part dataset, however with
+    some specific preprocessing steps, see `combine_dataset` and
+    `prepare_dataset` as well as the paper.
+
+    :param path: Path to the downloaded ShapeNet Part directory.
+    :param split: Which data split to process, either 'train', 'val', 'test'
+        or 'overfit'. Overfit dataset consists of the first instance in the
+        val dataset and can be used for sanity checks on the train/val
+        process.
+    :param size_sub: tdb
+    """
+    def __init__(self, path: Path = Path('hdf5_data'),
+                 split: str = 'train', size_sub: int = 16):
+        self.num_classes = 50
+        self.size_image = size_sub ** 2
+        combined_hdf5_file = Path('shapenet_combined.h5')
+        prepared_hdf5_file = Path('shapenet_prepared.h5')
+        if not combined_hdf5_file.exists():
+            combine_dataset(path, combined_hdf5_file)
+        if not prepared_hdf5_file.exists():
+            prepare_dataset(combined_hdf5_file, prepared_hdf5_file)
+
+        assert split in ['train', 'val', 'test', 'overfit'], \
+            ValueError("Invalid split")
+        self.data = h5py.File(path, 'r')
+        if split != 'overfit':
+            self.points_3d = torch.from_numpy(self.data[f"x_{split}"])
+            self.parts = torch.from_numpy(self.data[f"s_{split}"])
+            self.points_2d = torch.from_numpy(self.data[f"p_{split}"])
+        else:
+            points_3d = torch.from_numpy(self.data["x_val"])
+            parts = torch.from_numpy(self.data["s_val"])
+            points_2d = torch.from_numpy(self.data["p_val"])
+            self.points_3d = torch.unsqueeze(points_3d[0], 0)
+            self.parts = torch.unsqueeze(parts[0], 0)
+            self.points_2d = torch.unsqueeze(points_2d[0], 0)
+        assert len(self.points_3d) == len(self.parts) == len(self.points_2d), \
+            "Instance dimension of 3D points, parts and 2D points do not match"
+
+    def __getitem__(self, item) -> tuple[Tensor, Tensor]:
+        points_2d_item = self.points_2d[item]
+        points_3d_image = torch.zeros((self.size_image, self.size_image, 3),
+                                      dtype=torch.float32)
+        parts_image = torch.zeros((self.size_image, self.size_image, 3),
+                                  dtype=torch.float32) + self.num_classes
+        points_3d_image[points_2d_item[:, 0],
+                        points_2d_item[:, 1]] = self.points_3d[item]
+        parts_image[points_2d_item[:, 0],
+                    points_2d_item[:, 1]] = self.parts[item]
+        return points_3d_image, parts_image
+
+    def __len__(self) -> int:
+        return len(self.points_3d)
---
+++ prepare_dataset.py
@@ -0,0 +1,181 @@
+from pathlib import Path
+
+import h5py
+import numpy as np
+
+from utils import jitter_point_cloud, gpgl2_segmentation
+
+
+def combine_split(split: str, files: list[Path], result_file: h5py.File):
+    """
+    Combines all HDF5 files for a specific data split into a new combined HDF5
+    file.
+
+    :param split: Which data split to process, either 'train', 'val' or 'test'.
+    :param files: List of file paths of HDF5 files that belong to this split.
+    :param result_file: HDF5 file where the results should be written into.
+    """
+    assert split in ['train', 'val', 'test'], ValueError("Invalid split")
+
+    split_length = 0
+    for file in files:
+        with h5py.File(file, 'r') as f:
+            split_length += len(f['label'])
+    print(f"{split} set length: {split_length}")
+
+    points_3d = result_file.create_dataset(
+        f"x_{split}",
+        (split_length, 2048, 3),
+        dtype='<f4'
+    )
+    labels = result_file.create_dataset(
+        f"y_{split}",
+        (split_length, 1),
+        dtype='|u1'
+    )
+    parts = result_file.create_dataset(
+        f"s_{split}",
+        (split_length, 2048),
+        dtype='|u1'
+    )
+
+    offset = 0
+    for file in files:
+        with h5py.File(file, 'r') as f:
+            print(f"combining dataset: {file.name}")
+            dataset_length = len(f['label'])
+            points_3d[offset:offset + dataset_length] = f['data']
+            labels[offset:offset + dataset_length] = f['label']
+            parts[offset:offset + dataset_length] = f['pid']
+            offset += dataset_length
+
+
+def combine_dataset(data_path: Path, combined_hdf5_file: Path):
+    """
+    Combines all raw HDF5 data files for the ShapeNet Part dataset into one
+    combined HDF5 data file.
+
+    :param data_path: Path to the downloaded ShapeNet Part directory.
+    :param combined_hdf5_file: Path to the generated combined HDF5 file.
+    """
+    train_files = []
+    val_files = []
+    test_files = []
+    for file in sorted(data_path.iterdir()):
+        if file.name.startswith("ply_data_train"):
+            train_files.append(file)
+        elif file.name.startswith("ply_data_val"):
+            val_files.append(file)
+        elif file.name.startswith("ply_data_test"):
+            test_files.append(file)
+
+    with h5py.File(combined_hdf5_file, 'w') as result_file:
+        combine_split('train', train_files, result_file)
+        combine_split('val', val_files, result_file)
+        combine_split('test', test_files, result_file)
+
+
+def prepare_split(split: str, input_file: Path, result_file: h5py.File,
+                  num_repeats_train: int = 5,
+                  jitter_train_points: bool = True):
+    """
+    Prepares each split such that 3D point clouds with part annotations are
+    transformed into 2D images where pixels are assigned to 3D points with part
+    annotations. See the paper for more details.
+
+    :param split: Which data split to process, either 'train', 'val' or 'test'.
+    :param input_file: Path to the combined ShapeNet Part HDF5 data file.
+    :param result_file: HDF5 file where the results should be written into.
+    :param num_repeats_train: How many times the training data should be
+        repeatedly processed.
+    :param jitter_train_points: Weather the training data points should be
+        jittered with some noise.
+    """
+    assert split in ['train', 'val', 'test'], ValueError("Invalid split")
+
+    if split == 'train':
+        num_repeats = num_repeats_train
+    else:
+        num_repeats = 1
+
+    with h5py.File(input_file, 'r') as f:
+        num_instances, num_points = f[f"s_{split}"].shape
+        points_3d = f[f"x_{split}"][:]
+        labels = f[f"y_{split}"][:]
+        parts = f[f"s_{split}"][:]
+
+    result_points_3d = result_file.create_dataset(
+        f"x_{split}",
+        (num_instances * num_repeats, num_points, 3),
+        dtype='f'
+    )
+    result_labels = result_file.create_dataset(
+        f"y_{split}",
+        (num_instances * num_repeats),
+        dtype='i'
+    )
+    result_parts = result_file.create_dataset(
+        f"s_{split}",
+        (num_instances * num_repeats, num_points),
+        dtype='i'
+    )
+    result_points_2d = result_file.create_dataset(
+        f"p_{split}",
+        (num_instances * num_repeats, num_points, 2),
+        dtype='i'
+    )
+
+    sample_idx = 0
+    node_losses = []
+    for repeat_idx in range(num_repeats):
+        for instance_idx in range(num_instances):
+            current_points_3d = points_3d[instance_idx]
+            current_label = labels[instance_idx]
+            current_parts = parts[instance_idx]
+            current_points_3d = current_points_3d[np.newaxis, :, :]
+            if jitter_train_points and split == 'train':
+                current_points_3d = jitter_point_cloud(current_points_3d)
+            current_points_2d, node_loss = gpgl2_segmentation(
+                current_points_3d[0])
+            result_points_3d[sample_idx] = current_points_3d
+            result_labels[sample_idx] = current_label
+            result_parts[sample_idx] = current_parts
+            result_points_2d[sample_idx] = current_points_2d
+            sample_idx += 1
+            node_losses.append(node_loss)
+            print(f"Split {split}: iteration {sample_idx}, loss {node_loss}")
+
+    mean_node_loss = np.array(node_losses).mean()
+    result_file[f"num_repeats_{split}"] = num_repeats
+    result_file[f"mean node loss_{split}"] = mean_node_loss
+    print(f"{split} loss: {mean_node_loss}")
+
+
+def prepare_dataset(combined_hdf5_file: Path, prepared_hdf5_file: Path):
+    """
+    Prepares the combined HDF5 data with 3D point clouds with part annotations
+    into 2D images where pixels are assigned to 3D points with part
+    annotations. See the paper for more details.
+
+    :param combined_hdf5_file: Path to the combined HDF5 data file.
+    :param prepared_hdf5_file: Path to the prepared HDF5 data file.
+    """
+    with h5py.File(prepared_hdf5_file, 'w') as result_file:
+        prepare_split('train', combined_hdf5_file, result_file)
+        prepare_split('val', combined_hdf5_file, result_file)
+        prepare_split('test', combined_hdf5_file, result_file)
+
+
+def main():
+    """
+    Script that executes both the data combination and preparation step.
+    """
+    data_path = Path('hdf5_data')
+    combined_hdf5_file = Path('shapenet_combined.h5')
+    prepared_hdf5_file = Path('shapenet_prepared.h5')
+    combine_dataset(data_path, combined_hdf5_file)
+    prepare_dataset(combined_hdf5_file, prepared_hdf5_file)
+
+
+if __name__ == '__main__':
+    main()
